479a60563cb193eb7e22b9789b663eefd2cf9b67
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-11-04 15:01:27 -0800

    20 minutes of work

--- main.cpp
+++ main.cpp
@@ -8,12 +8,48 @@ IDE used: VSC*/
 #include <array>
 using namespace std;
 
-//Functions that do things
-void event_resolution(int, map<string, array<string,3>>);
-void print_trees(map<string, array<string,3>>);
+//Functions that handle the event resolutions based on a random number
+//Then we have a print function to display the trees.
+//The print function will likely need to break up trees based on type
+
+void event_Resolution(int, map<string, array<string,3>>);
+void print_Trees(map<string, array<string,3>>);
+string tree_Type();
+string fruit_Number();
+string tree_Age();
 
 
+//The main will handle the filling of the data
+//It should also handle the random event chance stuff. Ill give a 20%
+//chance for event to occur.
 int main()
 {
+    //This is going to be where the file input comes in.
+    //The file will simply just shove into a new map..
+    //The maps will likely be made by a loop just pushing in the values gotten from the text fiel
+    //And the individual things about the map will be put in by some randomized choices.
+    //The randomized choices will include the tree type, fruit it produced, and age.
+    //I'll have the randomized tree options be individual function calls to reduce the size of main.
 
 }
+
+void event_Resolution(int, map<string, array<string,3>>)
+{
+
+};
+void print_Trees(map<string, array<string,3>>)
+{
+
+};
+string tree_Type()
+{
+
+};
+string fruit_Number()
+{
+
+};
+string tree_Age()
+{
+
+};
