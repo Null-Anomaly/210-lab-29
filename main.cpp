d30ac6794fcce96c7223ee8f75daaf837ac7d369
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-11-04 14:47:09 -0800

    10 minutes of work

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*Comsc 210 | Lab 29 | Lawrence Bryant
+IDE used: VSC*/
+
+//Generic includes for this pseudocode.
+#include <iostream>
+#include <string>
+#include <map>
+#include <array>
+using namespace std;
+
+//Functions that do things
+void event_resolution(int, map<string, array<string,3>>);
+void print_trees(map<string, array<string,3>>);
+
+
+int main()
+{
+
+}
