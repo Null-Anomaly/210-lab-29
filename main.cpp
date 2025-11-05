51bc379e41f350a9ae95b51b306b99442f7e15e2
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-11-05 14:15:00 -0800

    40 minutes of work

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ IDE used: VSC*/
 #include <string>
 #include <map>
 #include <array>
-#include <ifstream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 //Functions that handle the event resolutions based on a random number
@@ -25,6 +27,7 @@ string tree_Age();
 //chance for event to occur.
 int main()
 {
+
     //This is going to be where the file input comes in.
     //The file will simply just shove into a new map..
     //The maps will likely be made by a loop just pushing in the values gotten from the text fiel
@@ -32,12 +35,16 @@ int main()
     //The randomized choices will include the tree type, fruit it produced, and age.
     //I'll have the randomized tree options be individual function calls to reduce the size of main.
 
+    //srand
+
     //Variables
     string treeput;
     string fruit;
     string num;
     string age;
     map<string, array<string,3>> farm;
+
+    ifstream fin("Outline.txt)");
 
     //When I have file input this will actually do something
     while(fin.good())
@@ -50,11 +57,42 @@ int main()
         //And so on and so forth.
     }
 
+    //Print initial conditions here. Then from there we begin doing the various loops
+    //So with this spot we start doing randomization
+    //Insert random integer here.
+
+    for(int i = 0; i < 25; i++)
+    {
+        //randomize each time. If under 20% do an if statement to put in a randomized event
+        //Otherwise, just increase age by 1, and make it so that the trees have a random number of fruit
+        
+    }
     return 0;
 }
 
-void event_Resolution(int, map<string, array<string,3>>)
+//Takes in a number, then spits out according to that number.
+void event_Resolution(int rando, map<string, array<string,3>> treee)
 {
+    if (rando == 1)
+    {
+
+    }
+    if (rando == 2)
+    {
+        
+    }
+    if (rando == 3)
+    {
+        
+    }
+    if (rando == 4)
+    {
+        
+    }
+    if (rando == 5)
+    {
+        
+    }
 
 };
 void print_Trees(map<string, array<string,3>>)
