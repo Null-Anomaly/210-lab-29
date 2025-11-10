4ceadeaa8176c27b65814c587c2ccc88b4a5de78
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-11-09 21:53:38 -0800

    Final version

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*Comsc 210 | Lab 29+30 | Lawrence Bryant
+/*Comsc 210 | Lab 29+30+31 | Lawrence Bryant
 IDE used: VSC*/
 
 //Generic includes for this pseudocode.
@@ -12,9 +12,6 @@ IDE used: VSC*/
 using namespace std;
 
 //Functions that handle the event resolutions based on a random number
-//Then we have a print function to display the trees.
-//The print function will likely need to break up trees based on type
-
 void event_Resolution(int, map<string, array<string,3>>&);
 void print_Trees(map<string, array<string,3>>);
 string tree_Type();
@@ -43,7 +40,7 @@ int main()
     ifstream fin("Values.txt)");
     fin.open("Values.txt");
 
-    //When I have file input this will actually do something
+    //While file input is good
     while(fin.good())
     {
         getline(fin, treeput);
@@ -51,12 +48,12 @@ int main()
         num = fruit_Number();
         age = tree_Age();
         farm[treeput] = {fruit, num, age};
-        //And so on and so forth.
     }
+
     //Print initial conditions here. Then from there we begin doing the various loops
     print_Trees(farm);
     
-
+    //The main loop that handles each year that passes.
     for(int i = 0; i < 25; i++)
     {
         randocool = rand() % 100;
@@ -64,19 +61,17 @@ int main()
         if(randocool <= 20)
         {
             event_Resolution(randocool,farm);
-           // cout << "Area 3a passed\n";
+
         }
         else
         {
             fruit_Change(farm, 0);
             age_Up(farm, 0);
-            //cout << "Area 3b passed\n";
         }
 
         cout << "Year: " << i + 1 << "\n";
         print_Trees(farm);
 
-        
     }
 
     cout << "Just here to look at all of the info. Just hit enter to close.\n";
@@ -88,7 +83,6 @@ int main()
 //Takes in a number, then spits out according to that number.
 void event_Resolution(int rando, map<string, array<string,3>> &treee)
 {
-    //cout << "Made it to Event Reso function\n";
     
     static bool pestecide_Used = NULL;
    
@@ -136,21 +130,17 @@ void event_Resolution(int rando, map<string, array<string,3>> &treee)
         //Droughts. Decrease Fruit count.
     }
 
-    cout << "Made it out of Event Reso function\n";
-
 };
 
 //Tree print function
 void print_Trees(map<string, array<string,3>> treee)
 {
-    cout << "Made it to print function\n";
     auto it = treee.begin();
     while(it != treee.end())
     {
         cout << "Tree ID: " << it->first << " | Type: " << it->second[0] << " | Fruit Count: " << it->second[1] << " | Age: " << it->second[2] << "\n";
         it++;
     };
-    cout << "Made it out of print function\n";
 
 };
 
@@ -174,12 +164,6 @@ array<string, 51> fruit = { "zero", "one", "two", "three", "four", "five", "six"
   "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty",};
 int randomized = rand() % 51;
 return fruit[randomized];
-
-    //This will randomly generate fruit number based on age and type.
-    //A number will be randomly generated. An array with like a hundred? Some numbe
-    //of values will be made and just randomly call on one of them to return the number
-    //I actually want as a string since I can't make an array two types without it
-    //being an object of some sort.
 
 };
 
@@ -206,7 +190,6 @@ void fruit_Change(map<string, array<string,3>> &treee,int disaster)
   "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty",
   "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty",};
 
-
     int randomization = 0;
     auto it = treee.begin();
     //Rain
@@ -225,6 +208,7 @@ void fruit_Change(map<string, array<string,3>> &treee,int disaster)
                 it->second[1] = fruit[randomization];
             }
         }
+
         //Fertilizer
         else if(disaster == 2)
         {
@@ -239,6 +223,7 @@ void fruit_Change(map<string, array<string,3>> &treee,int disaster)
                 it->second[1] = fruit[randomization];
             }
         }
+
         //Plague occurs
         else if(disaster == 4)
         {
@@ -253,7 +238,8 @@ void fruit_Change(map<string, array<string,3>> &treee,int disaster)
                 it->second[1] = fruit[randomization];
             }
         }
-        //Drout
+
+        //Drought
         else if(disaster == 5)
         {
             randomization = rand() % 50 - 10;
@@ -265,9 +251,9 @@ void fruit_Change(map<string, array<string,3>> &treee,int disaster)
             else
             {
                 it->second[1] = fruit[randomization];
-            }
-            
+            }   
         }
+
         //Plague but pesticides were used.
         else if(disaster == 6)
         {
@@ -282,6 +268,7 @@ void fruit_Change(map<string, array<string,3>> &treee,int disaster)
                 it->second[1] = fruit[randomization];
             }
         }
+        
         //Any other non-fruit affecting event case.
         else
         {
